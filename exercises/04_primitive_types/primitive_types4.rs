14806c350084e74f281d9c8f9ca0f74ad7751145
SrRecursive <rojohnibana@gmail.com>
2024-01-12 14:54:57 +0100

    Primitive_types completed!

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,10 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
-
+    let nice_slice = &a[1..4]; // Reference a from index [1, 4) (Includes 1, doesn't include 4)
     assert_eq!([2, 3, 4], nice_slice)
 }
