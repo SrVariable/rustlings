dc0f2e59e8b56e1ee0d8b66e193f6391becfe24f
SrRecursive <rojohnibana@gmail.com>
2024-01-12 13:24:26 +0100

    Variables completed!

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -5,9 +5,9 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
+    // Missing 'let' to create the variable: x = 5 -> let x = 5
+    // NOTE: x can have any type.
+    let x = 5;
     println!("x has the value {}", x);
 }
--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x: i32;
+    // Fix missing value: let x: i32 -> letx: i32 = 10
+    let x: i32 = 10;
     println!("Number {}", x);
 }
